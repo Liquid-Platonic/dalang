6c22f295219642be90b48766f74bf7a4132a9395
Panagiotis Vardanis <panagiotis.vardanis@utopiamusic.com>
2022-07-08 17:20:31 +0200

    add SpotifyConnection that establishes a connection with Spotify API

---
+++ dalang/crawling/spotifyconnection.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+class SpotifyConnection:
+    def __init__(self, client_id: str, client_secret: str) -> None:
+        self.spotify_connection: Optional[spotipy.Spotify] = None
+        self._get_spotify_connection(client_id, client_secret)
+
+    def __call__(self):
+        return self.spotify_connection
+
+    def _get_spotify_connection(
+        self, client_id: str = None, client_secret: str = None
+    ) -> spotipy.Spotify:
+        client_credentials_manager = SpotifyClientCredentials(
+            client_id=client_id, client_secret=client_secret
+        )
+        self.spotify_connection = spotipy.Spotify(
+            client_credentials_manager=client_credentials_manager
+        )
