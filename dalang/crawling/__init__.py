00ed917424b9b38041bc8f068232fbd1b3c80bb9
Panagiotis Vardanis <panagiotis.vardanis@utopiamusic.com>
2022-07-09 11:50:03 +0200

    init recommendations crawler

--- dalang/crawling/__init__.py
+++ dalang/crawling/__init__.py
@@ -1,8 +1,14 @@
 from dalang.config.configs import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 from dalang.crawling.spotifyconnection import SpotifyConnection
 from dalang.crawling.spotifyidcrawler import SpotifyIDCrawler
+from dalang.crawling.spotifyrecommendationscrawler import (
+    SpotifyRecommendationsCrawler,
+)
 
 spotify_connection = SpotifyConnection(
     SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 )
 spotify_id_crawler = SpotifyIDCrawler(spotify_connection)
+spotify_recommendations_crawler = SpotifyRecommendationsCrawler(
+    spotify_connection
+)
