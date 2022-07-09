bcfdb41d0f5506e442a28475f79bc1e6db2bbcb0
Nikos Atlas <nikatlas@gmail.com>
2022-07-09 23:21:24 +0300

    Messagedb (#25)
    * db
    
    * add on_message
    
    * db
    
    Co-authored-by: Grigoris Bourdalas <grigoris@Grigoriss-MacBook-Pro.local>

--- dalang/discordbot/main.py
+++ dalang/discordbot/main.py
@@ -1,9 +1,11 @@
+from discord import Message
 from dotenv import load_dotenv
 
 import dalang.config.configs as config
 import dalang.discordbot.commands
 import dalang.discordbot.views
 from dalang.discordbot.client import bot
+from dalang.discordbot.db import message_db
 
 load_dotenv()
 
@@ -11,6 +13,11 @@ load_dotenv()
 @bot.event
 async def on_ready():
     print(f"Connected as {bot.user}")
+
+
+@bot.event
+async def on_message(message: Message):
+    message_db(message.guild).add(message)
 
 
 bot.run(config.DISCORD_TOKEN)
