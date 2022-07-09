773600911eaedca0940c2454298821a1ca593892
Nikos Atlas <nikatlas@gmail.com>
2022-07-09 23:32:25 +0300

    complete control and domination (#27)

--- dalang/discordbot/youtube_to_genre_mood.py
+++ dalang/discordbot/youtube_to_genre_mood.py
@@ -4,12 +4,34 @@ from discord import TextChannel
 
 from dalang.crawling import spotify_id_crawler
 from dalang.discordbot.fetch_youtube_links_from_channel import (
+    fetch_title_from_link,
     fetch_youtube_links_from_channel,
 )
 from dalang.models import cyanite_model
 from dalang.postprocessing.averagepredictionsaggregator import (
     AveragePredictionsAggregator,
 )
+from dalang.tagging import TagPredictions
+
+
+def get_mood_from_link(
+    link: str,
+) -> (TagPredictions, TagPredictions, list, bool):
+    title = fetch_title_from_link(link)
+    spotify_ids = spotify_id_crawler.get_id_by_title(title)
+    if not spotify_ids:
+        return {}, {}, [], False
+    cyanite_tags = cyanite_model.predict(spotify_ids)
+
+    genres = {}
+    if cyanite_tags["genres"]:
+        genres = AveragePredictionsAggregator.aggregate(cyanite_tags["genres"])
+
+    moods = {}
+    if cyanite_tags["moods"]:
+        moods = AveragePredictionsAggregator.aggregate(cyanite_tags["moods"])
+
+    return genres, moods, spotify_ids, True
 
 
 async def youtube_to_genre_mood(
